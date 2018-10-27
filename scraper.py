ac62624c634190c66f1f0df1bba5a5651d04777d
mbaxada3 <daniel.ally@student.manchester.ac.uk>
2018-10-27 21:45:08 +0100

    Prototype article scraping stuff

--- scraper.py
+++ scraper.py
@@ -11,24 +11,27 @@ class StockCrawler(Spider):
         self.start_urls = urls
 
     def parse(self, response):
-        filename = response.url
-
-        with open(filename, 'wb') as f:
-            f.write(response.body)
 
         print(response)
-        result_links = response.css(".news-link::attr(href)").extract()
-        result_titles = response.css(".news-link::text").extract()
-        print(result_links + "\n" + result_titles)
+        # for article in results_links
+        #   article = xpath shit
+        #   result_articles =
 
+        result_articles = response.xpath('//div[@id="article-body"]/p/text()').extract()
+        print(result_articles)
+
+
+# Takes a stock name - Returns a dictionary of information to do sentiment analysis on
 def scrape(stock):
 
-    base_request = 'http://markets.businessinsider.com/news/'
-    urlsToPass = [base_request + stock, base_request + "intc"]
+    urlsToPass = [stock]
     process = CrawlerProcess({
         'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
     })
     process.crawl(StockCrawler, urlsToPass)
     process.start()
 
-scrape('amd')
+
+scrape('http://feeds.marketwatch.com/~r/marketwatch/marketpulse/~3/fldo14144Ng/story.aspx')
+
+
