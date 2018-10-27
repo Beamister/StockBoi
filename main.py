9b5c24a780ba2fe58bd2a924c117c26b117b2e1c
Luke Beamish <lukebeamish1@hotmail.co.uk>
2018-10-27 12:41:50 +0100

    Setting up flask sturcture

--- main.py
+++ main.py
@@ -4,5 +4,9 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def render_home():
     return render_template('index.html')
+
+@app.route('/result/<stock_name>')
+def render_result():
+    return render_template('results.html')
