4dc080bdfcdc2a8fe3bc321663a00c0f64de512e
Zara Robinson <zara.robinson@student.manchester.ac.uk>
2018-10-27 23:54:27 +0100

    working for 10 articles printed along with confidence score and negetivity level

--- main.py
+++ main.py
@@ -25,27 +25,39 @@ def render_result():
     if request.method=='GET':
         stock_dict = request.args
         scraped_data = scrape(stock_dict['stock_name'])
-        analysis_result = {'metadata':{'overall_score':-0.8},
-                           'articles':{0:{'headline':'Article headline 1',
+        analysis_result = {'metadata':{'overall_score':0.0},
+                           'articles':{ 0:{ 'headline':'Article headline 1',
                                             'text1':'this is the first text',
                                             'text2':'the second one',
                                             'text3':'and the third text',
                                             'date_time':'02_01_2018 17:50',
                                             'certainty':0.46},
-                                       1:{'headline':'Article headline 2',
+                                        1:{'headline':'Article headline 2',
                                             'text1':'text uno',
                                             'text2':'dos',
                                             'text3':'tres',
                                             'date_time':'02_01_2019 16:45',
                                             'certainty':0.63},
-                                       2:{'headline':'Article headline 3',
+                                        2:{'headline':'Article headline 3',
                                             'text1':'one',
                                             'text2':'two',
                                             'text3':'three',
                                             'date_time':'02_01_2019 16:45',
-                                            'certainty':0.88}
+                                            'certainty':0.88},
+                                        3:{'headline':'Article headline 1',
+                                            'text1':'this is the first text',
+                                            'text2':'the second one',
+                                            'text3':'and the third text',
+                                            'date_time':'02_01_2018 17:50',
+                                            'certainty':0.46},
+                                        4:{'headline':'Article headline 2',
+                                            'text1':'text uno',
+                                            'text2':'dos',
+                                            'text3':'tres',
+                                            'date_time':'02_01_2019 16:45',
+                                            'certainty':0.63}
                                       }
                           }
-        return render_template('results.html',stock_dict=stock_dict,analysis_result=analysis_result, str=str)
+        return render_template('results.html',stock_dict=stock_dict,analysis_result=analysis_result)
     else:
         render_template('error.html')
