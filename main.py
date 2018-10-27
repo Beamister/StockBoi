e0e0a1cb5ccdf5b13121b865ffe4662c71cb1056
Luke Beamish <lukebeamish1@hotmail.co.uk>
2018-10-27 12:52:24 +0100

    Added analyser and scraper

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import render_template
+from scraper import scrape
+from analyser import analyse
 
 app = Flask(__name__)
 
@@ -7,6 +9,8 @@ app = Flask(__name__)
 def render_home():
     return render_template('index.html')
 
-@app.route('/result/<stock_name>')
-def render_result():
-    return render_template('results.html')
+@app.route('/result/<stock')
+def render_result(stock=""):
+    scraped_data = scrape(stock)
+    analysis_result = analyse(scraped_data)
+    return render_template('results.html', suggestion=analysis_result)
