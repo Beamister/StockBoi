5083a6db534b0427859c533c02e8a5e0080d3dfe
Vlad Farcas <vladfarcas988@gmail.com>
2018-10-27 21:49:22 +0100

    Fix relative links. Comments.

--- scraper.py
+++ scraper.py
@@ -1,7 +1,7 @@
 from scrapy import *
 from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
 
+base_request = 'http://markets.businessinsider.com/news/'
 
 class StockCrawler(Spider):
     name = "stock_business_insider"
@@ -11,24 +11,32 @@ class StockCrawler(Spider):
         self.start_urls = urls
 
     def parse(self, response):
-        filename = response.url
 
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+        raw_links = response.css(".news-link::attr(href)").extract()
 
-        print(response)
-        result_links = response.css(".news-link::attr(href)").extract()
+        # Clean relative links out.
+        result_links = []
+        for s in raw_links:
+            if s[0:4] != "http":
+                s = base_request[0:-6] + s
+            result_links.append(s)
+        print(result_links)
+
         result_titles = response.css(".news-link::text").extract()
-        print(result_links + "\n" + result_titles)
+        print(result_titles)
+
 
 def scrape(stock):
 
-    base_request = 'http://markets.businessinsider.com/news/'
     urlsToPass = [base_request + stock, base_request + "intc"]
+
+    # Set the scraper up.
     process = CrawlerProcess({
         'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
     })
     process.crawl(StockCrawler, urlsToPass)
+
+    # Blocks until scraper finishes. Should also clean up after itself.
     process.start()
 
 scrape('amd')
