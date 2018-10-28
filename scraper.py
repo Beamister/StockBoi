3c806210003ccbace7c962f6f8ac08b294652816
Vlad Farcas <vladfarcas988@gmail.com>
2018-10-28 11:42:07 +0000

    Format output of data. Some no article issue.

--- scraper.py
+++ scraper.py
@@ -1,8 +1,11 @@
 from scrapy import *
 from scrapy.crawler import CrawlerProcess
-from time import sleep
 
 base_request = 'http://markets.businessinsider.com/news/'
+
+result_articles = {}
+article_links = []
+result_titles = []
 
 
 class StockCrawler(Spider):
@@ -10,17 +13,18 @@ class StockCrawler(Spider):
     name = "stock_business_insider"
     start_urls = []
     seen_before = []
-    result_articles = []
 
     def __init__(self, urls):
         self.start_urls = urls
 
-    def parse_text(self, response):
-        yield
-
     def parse_article(self, response):
-        print("Xpath at response: " + response.url)
-        all_results = response.xpath('//p[string-length(text()) > 150]/text()').extract()
+
+        # print("Xpath at response: " + response.url)
+
+        all_results = response.xpath('//p[string-length(text()) > 20]/text()').extract()
+
+        # print(response.url)
+
         for string in all_results:
             if string in self.seen_before:
                 all_results.remove(string)
@@ -28,44 +32,40 @@ class StockCrawler(Spider):
                 self.seen_before.append(string)
 
         all_results = " ".join(all_results)
-
-        result = {"One": all_results}
-        print("resulty boi " + result["One"])
-        return result
+        result_articles[response.url] = all_results
 
 
     def parse(self, response):
 
         raw_links = response.css(".news-link::attr(href)").extract()
 
-        # Clean relative links out.
-        article_links = []
+        titles = response.css(".news-link::text").extract()
+
+        # print(len(titles))
+
+        for i in range(0, len(titles)):
+            result_titles.append(titles[i])
+
+        # Clean out relative links
         for s in raw_links:
             if s[0:4] != "http":
                 s = base_request[0:-6] + s
             article_links.append(s)
-        # print(article_links)
+            result_articles.update({s : ""})
 
-        result_titles = response.css(".news-link::text").extract()
+
         # print("result titles")
         # print(result_titles)
 
-        result_articles = []
-
         for link in article_links:
-            sleep(0.5)
-            print("link:" + link)
-            one_article = yield Request(link, self.parse_article)
-            print(one_article)
-
-        print("result articles")
-        print(result_articles)
-
+            request = Request(link, self.parse_article)
+            request.meta['dont_redirect'] = True
+            yield request
 
 # Takes a stock name - Returns a dictionary of information to do sentiment analysis on
 def scrape_stock(stock):
 
-    urls_to_pass = [base_request + stock, base_request + "intc"]
+    urls_to_pass = [base_request + stock]
 
     # Set the scraper up.
     process = CrawlerProcess({
@@ -75,5 +75,21 @@ def scrape_stock(stock):
 
     # Blocks until scraper finishes. Should also clean up after itself.
     process.start()
+    article_count = len(result_articles)
 
-scrape_stock('amd')
+    # print(article_count)
+    # print(result_articles)
+    # print(len(result_titles))
+
+    data = {"metadata": {"count": article_count, "name": stock}}
+    i = 0
+    for s in result_articles.keys():
+        title = result_titles[i]
+        link = s
+        article = result_articles[link]
+        toAdd = {"link": link, "title": title, "article": article}
+        data[i] = toAdd
+        i += 1
+
+    # print(data)
+    return data
